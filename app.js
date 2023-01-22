8c3287733e0fca48cbff4f7e496a65c0e808fa82
Quadatz <quadath@gmail.com>
2023-01-22 19:35:57 +0300

    mongoose

--- app.js
+++ app.js
@@ -1,106 +1,83 @@
 const express = require('express')
-const { ObjectId } = require('mongodb')
-const { connectToDb, getDb } = require('./db')
+const mongoose = require('mongoose')
+const Movie = require('./models/movie')
 
 const PORT = 3000;
+const URL = "mongodb://127.0.0.1:27017/moviebox"
+
+mongoose.set('strictQuery', false)
 
 const app = express();
 app.use(express.json())
 
-let db;
+mongoose.connect(URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(`DB connection error: ${err}`))
 
-connectToDb((err) => {
-    if (!err) {
-        app.listen(PORT, (err) => {
-            err ? console.log(err) : console.log(`Listening on port ${PORT}`);
-            db = getDb();
-        })
+app.listen(PORT, (err) => {
+    if (err) {
+        throw err;
     } else {
-        console.log(`DB error ${err}`)
+        console.log(`App is running on port ${PORT}`)
     }
 })
 
 const handleError = (res, error) => {
-    res.status(500).json({error})
+    res.status(500).json({
+        error
+    })
 }
 
 app.get('/movies', (req, res) => {
-    const movies = [];
-
-    db.collection('movies')
-        .find()
-        .sort({title: 1})
-        .forEach((movie) => movies.push(movie))
-        .then(() => {
-
-            res.json(movies)
+    Movie.find()
+        .sort({
+            title: 1
+        })
+        .then((movies) => {
+            res.status(200)
+                .json(movies)
         })
         .catch(() => handleError(res, "Error"))
 })
 
 app.get('/movies/:id', (req, res) => {
-    if(ObjectId.isValid(req.params.id)) {
-        db.collection('movies')
-            .findOne({_id: ObjectId(req.params.id)})
-            .then((doc) => {
-                res.status(200)
-                .json(doc)
-            })
-            .catch(() => handleError(res, "Error"))
-    } else {
-        handleError(res, "Wrong id")
-    }
-})
-
-app.get('/liya', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': "text/html;charset=utf-8"
-    })
-    res.end(`<img src="https://i.imgur.com/GUqd2YO.png"/>`)
+    Movie
+        .findById(req.params.id)
+        .then((movie) => {
+            res.status(200)
+                .json(movie)
+        })
+        .catch(() => handleError(res, "Error"))
 })
 
 app.delete('/movies/:id', (req, res) => {
-    if(ObjectId.isValid(req.params.id)) {
-        db.collection('movies')
-            .deleteOne({_id: ObjectId(req.params.id)})
+        Movie
+            .findByIdAndDelete(req.params.id)
             .then((result) => {
                 res.status(200)
-                .json(result)
+                    .json(result)
             })
             .catch(() => handleError(res, "Error"))
-    } else {
-        handleError(res, "Wrong id")
-    }
 })
 
 app.post('/movies', (req, res) => {
-    db.collection('movies')
-    .insertOne(req.body)
-    .then((result) => {
-        res.status(201)
-        .json(result)
-    })
-    .catch(() => handleError(res, "Error POST"))
+    const movie = new Movie(req.body);
+
+    movie
+        .save()
+        .then((result) => {
+            res.status(201)
+                .json(result)
+        })
+        .catch(() => handleError(res, "Error POST"))
 });
 
-app.get('/liya', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': "text/html;charset=utf-8"
-    })
-    res.end(`<img src="https://i.imgur.com/GUqd2YO.png"/>`)
-})
-
 app.patch('/movies/:id', (req, res) => {
-    if(ObjectId.isValid(req.params.id)) {
-        db.collection('movies')
-        .updateOne({_id: ObjectId(req.params.id)}, {$set: req.body  })
+    Movie
+        .findByIdAndUpdate(req.params.id, req.body)
         .then((result) => {
             res.status(200)
-            .json(result)
+                .json(result)
         })
         .catch(() => handleError(res, "Error PATCH"))
-    }
-    else {
-        handleError(res, "Invalid ID")
-    }
 })
