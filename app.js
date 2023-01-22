e3f50fae5c8f9302c1e580e29ac455e6bb19c4a0
Quadatz <quadath@gmail.com>
2023-01-22 17:22:11 +0300

    getting data

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
-const {connectToDb, getDb} = require('./db')
+const {
+    connectToDb,
+    getDb
+} = require('./db')
 
 const PORT = 3000;
 
@@ -16,4 +19,36 @@ connectToDb((err) => {
     } else {
         console.log(`DB error ${err}`)
     }
+})
+
+app.get('/movies', (req, res) => {
+    const movies = [];
+
+    db.collection('movies')
+        .find()
+        .sort({title: 1})
+        .forEach((movie) => movies.push(movie))
+        .then(() => {
+
+            res.json(movies)
+        })
+        .catch(() => {
+            res.status(500)
+                .json({
+                    error: "Something went wrong..."
+                })
+        })
+})
+app.get('/pidori', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.end('<h1>Пидоры ёбаные ненавижу вас сукаа!11!!!</h1>')
+})
+
+app.get('/anya', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.end('<h1>Привет, Аня <3</h1>')
 })
